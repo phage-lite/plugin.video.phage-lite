d76dc20276bb0f92852a06257ce3b8bdeb826838
phage <28393493+phage-zn@users.noreply.github.com>
2026-06-06 19:49:00 +0300

    add imports

---
+++ .nvim/lsp/pyright.lua
@@ -0,0 +1,28 @@
+local cfg = {
+    pyright = {
+        settings = {
+            pyright = {
+                analysis = {
+                    typeCheckingMode = "standard",
+                    autoSearchPaths = true,
+                    useLibraryCodeForTypes = true,
+                },
+                inlayHints = {
+                    variableTypes = true,
+                    returnTypes = true,
+                    callArgumentNames = true,
+                    pytestParameters = true,
+                },
+            },
+        },
+    },
+    ruff = {
+        on_attach = function(client)
+            client.server_capabilities.hoverProvider = false
+        end,
+    },
+}
+
+for server_name, server_config in pairs(cfg) do
+    vim.lsp.config(server_name, server_config)
+end
